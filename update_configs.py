267299dd382640af8ba96c86d9414aeeb5470961
nzea243 <vladneza6@gmail.com>
2026-08-13 00:11:53 +0300

    Refactor announcement message in build_header
    Updated the announcement format to include line breaks.

--- update_configs.py
+++ update_configs.py
@@ -11,7 +11,11 @@ def build_header():
     now = datetime.now(timezone.utc).strftime('%H:%M %d.%m.%Y UTC')
     return f"""\
 #profile-title: nzea234 wl 2.0
-#announce: Последний апдейт на GitHub: {now} | Не работает — обнови подписку | Версия 2.0 | ВОССТАНИЕ
+#announce:
+Последний апдейт на GitHub: {now}
+Не работает — обнови подписку
+Версия 2.0
+ВОССТАНИЕ
 #support-url: https://t.me/nzea_tri_bykvi
 #profile-web-page-url:https://t.me/send?start=IV9P4rO9112W
 #profile-update-interval: 1
